9d13fd9bcd19076fd53b29e54301f049ddb114f5
B1229021 <144312167+B1229021@users.noreply.github.com>
2025-05-07 15:15:22 +0800

    Add files via upload

---
+++ HW04/react_datagrid/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DataTable from './data';
+
+function App() { 
+  return ( 
+    <div className="App"> 
+      <div>
+        <DataTable />
+      </div> 
+    </div> 
+  ); 
+} 
+
+export default App;
