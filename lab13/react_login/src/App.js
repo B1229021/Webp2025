85b7c02582908ec9ce1d283d2da7df99b5603078
B1229021 <144312167+B1229021@users.noreply.github.com>
2025-05-07 14:39:41 +0800

    Add files via upload

---
+++ lab13/react_login/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+// import logo from './logo.svg';
+// import MultiButton from './cgu_multiButton'
+// import HelloCGU from './cgu_hello'
+
+import CGU_Login  from './SignInSide'
+
+
+function App() { 
+  return ( 
+    <div className="App"> 
+      <div>
+        {/* { HelloCGU() }  */}
+        {/* { MultiButton(10) }  */}
+        {CGU_Login()}
+      </div> 
+    </div> 
+  ); 
+} 
+
+
+// const changeText=(event)=>{ 
+//   console.log(event.target) 
+//   event.target.innerText = event.target.innerText + "被點了" 
+// } 
+
+// function App() { 
+//   return ( 
+//     <div className="App"> 
+// <h1 style = { styleArgument } onClick= {changeText}> hello CGU!!  
+// </h1> 
+//     </div> 
+//   ); 
+// } 
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+
+      
+//     </div>
+
+    
+    
+
+//   );
+// }
